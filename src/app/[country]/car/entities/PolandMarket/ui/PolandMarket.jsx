b02761c0ddd33f97beed5ed4a807f4241e9cb210
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-01-14 21:42:46 +0600

    save

--- src/app/[country]/car/entities/PolandMarket/ui/PolandMarket.jsx
+++ src/app/[country]/car/entities/PolandMarket/ui/PolandMarket.jsx
@@ -32,11 +32,12 @@ const PolandMarket = () => {
       <div className="">
         <h2 className="text-title tw-mb-[16px]">Poland Market</h2>
       </div>
-      <ul className="tw-flex tw-flex-col tw-gap-[14px] tw-mb-[49px]">
+      <ul className="tw-flex tw-flex-col tw-gap-[14px]">
         {poland.map((item, idx) => {
           return <ListItem key={idx} label={item.label} value={item.value} icon={item.icon} />;
         })}
       </ul>
+      <hr className="tw-my-[25px]" />
       <div className="tw-flex tw-justify-between">
         <span className="tw-text-[18px] tw-text-[#191919]">Зазор от рынка</span>
         <span className="tw-text-[20px] tw-text-[#3E73CF]">$17,000</span>
--- src/components/ListItem/ui/ListItem.jsx
+++ src/components/ListItem/ui/ListItem.jsx
@@ -6,7 +6,8 @@ const ListItem = ({ label, icon, value }) => {
   return (
     <li className="list-item">
       <span className="list-item-label">
-        {label} {icon ? icon : ""}
+        <span className="tw-text-[16px]"> {label} </span>
+        <span className="tw-ml-[6px]">{icon ? icon : ""}</span>
       </span>
       <span className="list-item-border"></span>
       <span className="list-item-value">{value}</span>
