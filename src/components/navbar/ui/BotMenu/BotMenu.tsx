4cbe6351241bee4b1945095c60093eeff0030801
Marsel <marselbisengaliev1@gmail.com>
2025-01-15 23:46:57 +0500

    final version of finder

--- src/components/navbar/ui/BotMenu/BotMenu.tsx
+++ src/components/navbar/ui/BotMenu/BotMenu.tsx
@@ -9,6 +9,32 @@ const BotMenu = ({ parsedMakes }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(null); // Хранит ключ текущего открытого элемента
   const menuRef = useRef(null); // Реф для меню
+  const estimatedCostItems = [
+    { label: "To 2000$", link: "/listings?estimated_cost_max=2000" },
+    { label: "To 5000$", link: "/listings?estimated_cost_max=5000" },
+    { label: "To 10000$", link: "/listings?estimated_cost_max=10000" },
+    { label: "To 15000$", link: "/listings?estimated_cost_max=15000" },
+    { label: "To 20000$", link: "/listings?estimated_cost_max=20000" },
+    { label: "To 30000$", link: "/listings?estimated_cost_max=30000" },
+    { label: "To 50000$", link: "/listings?estimated_cost_max=50000" },
+    { label: "To 100000$", link: "/listings?estimated_cost_max=100000" },
+    { label: "To 200000$", link: "/listings?estimated_cost_max=200000" },
+  ];
+
+  const featuredItems = [
+    {
+      label: "Finished today",
+      link: "/listings?status=today"
+    },
+    {
+      label: "Opened",
+      link: "/listings?status=opened"
+    },
+    {
+      label: "Buy now",
+      link: "/listings?buy_now=true"
+    },
+  ];
 
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
@@ -61,7 +87,7 @@ const BotMenu = ({ parsedMakes }) => {
                   <div className={css.item__content}>
                     <h3>Popular Makes</h3>
                     <div className={css.list}>
-                      {parsedMakes.map((make) => (
+                      {parsedMakes.map((make: any) => (
                         <div key={make.id} className={css.element}>
                           <b>{make.name}</b>
                           <div className={css.dots}></div>
@@ -69,60 +95,62 @@ const BotMenu = ({ parsedMakes }) => {
                         </div>
                       ))}
                     </div>
+                    <a href="/listings"><span>View More</span> <i className="ri-arrow-right-fill"></i></a>
                   </div>
                 )}
               </div>
               <div className={css.item}>
                 <div
                   className={css.item__header}
-                  onClick={() => toggleItem("vehicleTypes")}
+                  onClick={() => toggleItem("estimatedCost")}
                 >
-                  <span>Vehicle Types</span>
+                  <span>Estimated cost</span>
                   <i
                     className={`ri-arrow-${activeItem === "vehicleTypes" ? "up" : "right"}-s-line`}
                   ></i>
                 </div>
-                {activeItem === "vehicleTypes" && (
-                  <div className={css.item__content}>
-                    <h3>Vehicle Types</h3>
+                {activeItem === "estimatedCost" && (
+                  <div className={`${css.item__content + " " + css.vehicleTypes}`}>
+                    <h3>Estimated cost</h3>
                     <div className={css.list}>
-                      {/* Добавьте содержимое для Vehicle Types */}
-                      <span>Vehicle types content goes here</span>
+                      {estimatedCostItems.length > 0 && estimatedCostItems.map((subItem, index) => (
+                        <a
+                          key={index}
+                          href={subItem.link}
+                          className="tw-text-white tw-bg-blue-700 tw-hover:bg-blue-800 tw-focus:ring-4 tw-focus:ring-blue-300 tw-font-medium tw-rounded-lg tw-text-sm tw-px-5 tw-py-2.5 tw-dark:bg-blue-600 tw-dark:hover:bg-blue-700 tw-focus:outline-none tw-dark:focus:ring-blue-800"
+                        >
+                          {subItem.label}
+                        </a>
+                      ))}
                     </div>
                   </div>
+
                 )}
               </div>
               <div className={css.item}>
                 <div
                   className={css.item__header}
-                  onClick={() => toggleItem("title")}
+                  onClick={() => toggleItem("Featured")}
                 >
-                  <span>Title</span>
+                  <span>Featured</span>
                   <i
-                    className={`ri-arrow-${activeItem === "title" ? "up" : "right"}-s-line`}
+                    className={`ri-arrow-${activeItem === "Featured" ? "up" : "right"}-s-line`}
                   ></i>
                 </div>
-                {activeItem === "title" && (
+                {activeItem === "Featured" && (
                   <div className={css.item__content}>
-                    <h3>Title Content</h3>
-                    {/* Добавьте содержимое для Title */}
-                  </div>
-                )}
-              </div>
-              <div className={css.item}>
-                <div
-                  className={css.item__header}
-                  onClick={() => toggleItem("featuredLots")}
-                >
-                  <span>Featured lots</span>
-                  <i
-                    className={`ri-arrow-${activeItem === "featuredLots" ? "up" : "right"}-s-line`}
-                  ></i>
-                </div>
-                {activeItem === "featuredLots" && (
-                  <div className={css.item__content}>
-                    <h3>Featured Lots</h3>
-                    {/* Добавьте содержимое для Featured Lots */}
+                    <h3>Featured</h3>
+                    <div className={css.list}>
+                      {featuredItems.length > 0 && featuredItems.map((subItem, index) => (
+                        <a
+                          key={index}
+                          href={subItem.link}
+                          className="tw-text-white tw-bg-blue-700 tw-hover:bg-blue-800 tw-focus:ring-4 tw-focus:ring-blue-300 tw-font-medium tw-rounded-lg tw-text-sm tw-px-5 tw-py-2.5 tw-dark:bg-blue-600 tw-dark:hover:bg-blue-700 tw-focus:outline-none tw-dark:focus:ring-blue-800"
+                        >
+                          {subItem.label}
+                        </a>
+                      ))}
+                    </div>
                   </div>
                 )}
               </div>
