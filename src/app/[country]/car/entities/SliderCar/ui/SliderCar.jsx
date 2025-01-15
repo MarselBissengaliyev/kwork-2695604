9af1de475d38718b5288da6d37f625a99ee31917
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-01-15 15:01:10 +0600

    save

--- src/app/[country]/car/entities/SliderCar/ui/SliderCar.jsx
+++ src/app/[country]/car/entities/SliderCar/ui/SliderCar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+import rrrr from "../../../shared/img/arrow-down.svg";
+
 const SliderCar = () => {
-  return <div className="tw-w-full tw-max-w-[760px] tw-h-[650px] tw-bg-indigo-700">SliderCar</div>;
+  return (
+    <div>
+      {/* Фото */}
+      <a data-fancybox="gallery" href="/path/to/image1.jpg">
+        <img src={rrrr} alt="Image 1" style={{ width: "150px", height: "auto" }} />
+      </a>
+
+      {/* Видео */}
+      <a data-fancybox="gallery" href="https://www.example.com/video.mp4" data-caption="Video Example">
+        <img src={rrrr} alt="Video Thumbnail" style={{ width: "150px", height: "auto" }} />
+      </a>
+
+      {/* Iframe */}
+      <a data-fancybox="gallery" data-type="iframe" href="https://example.com/panorama" data-caption="Panorama Example">
+        <img src={rrrr} alt="Iframe Thumbnail" style={{ width: "150px", height: "auto" }} />
+      </a>
+    </div>
+  );
 };
 
 export default SliderCar;
