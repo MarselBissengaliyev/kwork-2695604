0eb20f09e2ac7f26af17fa44a47115004c6a69bc
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-01-17 21:45:49 +0600

    add history card

--- src/app/[country]/car/entities/AccordionHistory/models/HistoryCard.jsx
+++ src/app/[country]/car/entities/AccordionHistory/models/HistoryCard.jsx
@@ -5,19 +5,19 @@ import car from "../../../shared/img/carlider.png";
 
 const HistoryCard = () => {
   return (
-    <div className="tw-w-full tw-flex tw-gap-[20px] tw-rounded-[10px] tw-py-[20px] tw-px-[25px] tw-bg-[#fff]">
+    <div className="tw-w-full tw-flex tw-gap-[20px] tw-rounded-[10px] tw-py-[10px] tablet:tw-py-[20px] tw-px-[15px] tablet:tw-px-[25px] tw-bg-[#fff]">
       <div className="">
         <Image className="tw-rounded-[10px] tw-w-[80px]" src={car} alt="" />
       </div>
-      <div className="tw-flex  tw-gap-[20px]">
-        <div className="tw-flex tw-flex-col ">
+      <div className="tw-flex tw-gap-[10px] tablet:tw-gap-[65px]  desktop:tw-gap-[20px]">
+        <div className="tw-flex table:tw-flex-col ">
           <div className="tw-flex tw-flex-col">
             {" "}
             <span className="tw-text-[13px] tablet:tw-text-[14px] tw-text-[#8C8C8C]">Final Bid</span>
             <span className="tw-text-[14px] tablet:tw-text-[16px]">$2,500</span>
           </div>
         </div>
-        <div className="tw-flex tw-flex-col">
+        <div className="tw-flex tw-flex-col tw-gap-[20px]  tablet:tw-gap-[65px] desktop:tw-gap-[20px] tablet:tw-flex-row desktop:tw-flex-col">
           <div className="tw-flex tw-flex-col">
             {" "}
             <span className="tw-text-[13px] tablet:tw-text-[14px] tw-text-[#8C8C8C]">Mileage</span>
