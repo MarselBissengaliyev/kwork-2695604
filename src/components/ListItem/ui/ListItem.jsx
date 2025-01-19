2e1d30ce317772576112564cc8727f40dfa1ed84
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-01-19 12:44:04 +0600

    refactor copy text

--- src/components/ListItem/ui/ListItem.jsx
+++ src/components/ListItem/ui/ListItem.jsx
@@ -6,11 +6,18 @@ const ListItem = ({ label, icon, value }) => {
   return (
     <li className="list-item">
       <span className="list-item-label">
-        <span className="list-item-label_title"> {label} </span>
+        <span className={` ${label === "Add Shipping" ? "tw-text-[#3E73CF]" : " "} list-item-label_title`}>
+          {" "}
+          {label}{" "}
+        </span>
         <span className={`tw-ml-[6px] ${icon ? "" : "tw-hidden"} `}>{icon ? icon : ""}</span>
       </span>
       <span className="list-item-border"></span>
-      <span className="list-item-value">{value}</span>
+      <span
+        className={` ${value === "YES" ? "tw-text-[#3E73CF]" : " "} ${value === "OutBid" ? "tw-text-[#E3433A]" : " "} list-item-value`}
+      >
+        {value}
+      </span>
     </li>
   );
 };
