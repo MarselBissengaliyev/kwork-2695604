d058cd6400839444e5ab85aba358898d9e043aa5
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2025-01-20 16:09:33 +0500

    feat: create Watchlist Block

--- src/app/[country]/dashboard/Watchlist/widgets/WatchlistBlock/WatchlistBlock.tsx
+++ src/app/[country]/dashboard/Watchlist/widgets/WatchlistBlock/WatchlistBlock.tsx
@@ -1,27 +1,136 @@
 import React from 'react'
 import Container from '@/components/Container/ui/Container'
-import CarCardsSlider from '../../../../../../components/Sliders/CarCardsSlider/CarCardsSlider'
-import WatchListCard from '../../entities/WatchlistCard/WatchListCard'
+import WatchListCard from '../../entities/WatchlistCard/WatchlistCard'
+import PaginationBlock from '../../../entities/paginationBlock/PaginationBlock'
 
 const WatchlistBlock = () => {
-    const imgs = [
+
+    const Cards = [
+
         {
-            alt: "watch",
-            src: "/images/dashboard/icons/car1.png"
+            title: "2013 Chevrolet Impala Ls 3.6L",
+            Lot: "#30874242",
+            Vin: "1D7RB1GP6AS109178",
+            List: [
+                {label: "Mileage", value: "198,239"},
+                {label: "Damage", value: "frontend"},
+                {label: "Location", value: "PA-Harrisburg"},
+                {label: "Sale Date", value: "06/13/2022"},
+                {label: "Doc. Type", value: "Clean Title"},
+            ],
+            CurrentBid: "131",
+            BuyBid: "2,850",
+            imgs: [
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car1.png"
+                },
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car2.png"
+                },
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car3.png"
+                }
+            ],
         },
         {
-            alt: "watch",
-            src: "/images/dashboard/icons/car2.png"
+            title: "2013 Chevrolet Impala Ls 3.6L",
+            Lot: "#30874242",
+            Vin: "1D7RB1GP6AS109178",
+            List: [
+                {label: "Mileage", value: "198,239"},
+                {label: "Damage", value: "frontend"},
+                {label: "Location", value: "PA-Harrisburg"},
+                {label: "Sale Date", value: "06/13/2022"},
+                {label: "Doc. Type", value: "Clean Title"},
+            ],
+            CurrentBid: "131",
+            BuyBid: "2,850",
+            imgs: [
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car1.png"
+                },
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car2.png"
+                },
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car3.png"
+                }
+            ],
         },
         {
-            alt: "watch",
-            src: "/images/dashboard/icons/car3.png"
-        }
+            title: "2013 Chevrolet Impala Ls 3.6L",
+            Lot: "#30874242",
+            Vin: "1D7RB1GP6AS109178",
+            List: [
+                {label: "Mileage", value: "198,239"},
+                {label: "Damage", value: "frontend"},
+                {label: "Location", value: "PA-Harrisburg"},
+                {label: "Sale Date", value: "06/13/2022"},
+                {label: "Doc. Type", value: "Clean Title"},
+            ],
+            CurrentBid: "131",
+            BuyBid: "2,850",
+            imgs: [
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car1.png"
+                },
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car2.png"
+                },
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car3.png"
+                }
+            ],
+        },
+        {
+            title: "2013 Chevrolet Impala Ls 3.6L",
+            Lot: "#30874242",
+            Vin: "1D7RB1GP6AS109178",
+            List: [
+                {label: "Mileage", value: "198,239"},
+                {label: "Damage", value: "frontend"},
+                {label: "Location", value: "PA-Harrisburg"},
+                {label: "Sale Date", value: "06/13/2022"},
+                {label: "Doc. Type", value: "Clean Title"},
+            ],
+            CurrentBid: "131",
+            BuyBid: "2,850",
+            imgs: [
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car1.png"
+                },
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car2.png"
+                },
+                {
+                    alt: "watch",
+                    src: "/images/dashboard/icons/car3.png"
+                }
+            ],
+        },
     ]
 
   return (
-    <Container>
-        <WatchListCard/>
+    <Container className={""}>
+        <div className='tw-grid tw-grid-cols-4 tw-gap-[20px]'>
+            {Cards.map((card) => <WatchListCard card={card}/> )}
+            
+        </div>
+
+        <div className='tw-flex tw-justify-center tw-items-center tw-my-[35px]'>
+            <PaginationBlock currentPage={1} totalPages={10}/>
+        </div>
     </Container>
   )
 }
